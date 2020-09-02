2c601a6327bcde7506cd7b350582c399c41fcdb4
Ashish <ash.ishu025g@gmail.com>
2020-09-02 20:14:28 +0530

    context and scope

--- CoreJs/ScopeAndContext.js
+++ CoreJs/ScopeAndContext.js
@@ -1,9 +1,22 @@
 // scope and context and execution context: 
-myName = "Dixon"
+myName = "Dixon"; //global scope variable
 function printName(params) {
     console.log("Param "+ params);
     console.log("My Name "+ this.myName);
     console.log("Scope (this) "+ this)
 }
 
-printName("Zhongjie");//invocation
+printName("Zhongjie");//invocation : context for this invocation belongs to global or window (if in browser)
+
+
+//context object (this) belongs to User
+let user = {
+    name : "Dixon", 
+    age : 25,
+    getUserName : function () {
+        console.log(this.name);
+        console.log("Context of getUserName ",this);
+    }
+}
+
+user.getUserName();
