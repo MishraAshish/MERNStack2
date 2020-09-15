247ea21ba2504075c28fce97f4cbaf3e79100e44
Ashish <ash.ishu025g@gmail.com>
2020-09-15 21:15:23 +0530

    practice

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -4,7 +4,7 @@ let app = require("express"); //just name of module installed or core modules
 global.port = process.env.port || 9090;
 
 let expressApp = app(); //initializing the express app
-let adminApp = app(); //sub application to serve requests coming to localhost:9090/admin
+let adminApp = app(); //sub application to serve requests coming to localhost:9090/admin - mounting
 
 expressApp.use('/static', app.static('Public')); // serve static files like images css using static middle ware
 
@@ -19,7 +19,7 @@ expressApp.get("/hello",(req, res)=>{
     })
 });
 
-expressApp.get("/html/:id",(req, res)=>{
+expressApp.get("/html/:id/:name",(req, res)=>{
     // console.log("req", req);
     console.log("query string data "+ JSON.stringify(req.query));
     console.log("params in api "+ JSON.stringify(req.params));
@@ -38,10 +38,17 @@ adminApp.get('/', function (req, res) {
 })
 
 
-// expressApp.get("*",(req, res)=>{
-//     res.send("Default! if nothing matched to the route")
-//     //res.sendfile(__dirname+"/Test.js")
-// });
+expressApp.get("*",(req, res)=>{
+    res.send("Default! if nothing matched to the route")
+    //res.sendfile(__dirname+"/Test.js")
+});
 
 console.log("server is listening at port : "+global.port)
 expressApp.listen(global.port);
+
+
+
+//1. create an api with mounted app (say adminuser)  -api - servejs
+//2. send a static file with name staticjs which has consol.log("Your Name")                                                  
+//3. use query string and params in the same api or in different
+//4. query string should contain user details 
